c7387d25488ae58a0ff258bcd471295e3148a036
RIBADISTRO <ribali0.96@gmail.com>
2020-02-01 02:39:34 -0600

    Actualiza la docmunetacion con mas script

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 
 echo -n "Digame su nombre:"
 read NOMBRE
-
+#Falta comentar aqui
 read -p "Y su apellido: " APELLIDO
 printf "Hola %s %s" $NOMBRE $APELLIDO
 
---
+++ script1.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Mal equipo mas echo "
+
+# Esta parte esta mal
+#Preparar equipo
